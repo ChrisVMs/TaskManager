67399c82885a1ac34fef3bc1562cc39f860c4d79
kevin__vargas <kevin__vargas@outlook.com>
2024-11-19 23:39:07 -0500

    Agregar archivos de proyecto.

---
+++ core/common/Enums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.core.common
+{
+    public class Enums
+    {
+
+        public enum Role
+        {
+            Jefatura = 1,
+            Empleado = 2
+        }
+
+        public enum EstadoTarea
+        {
+            Pendiente = 1,
+            EnProgreso = 2,
+            Finalizado = 3
+        }
+
+    }
+}
---
+++ core/model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.core.common;
+
+namespace TaskManager.core.model
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public Enums.Role role { get; set; }
+    }
+}
